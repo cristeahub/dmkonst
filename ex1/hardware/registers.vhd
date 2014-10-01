f34af521f33b923f6da9f54239ea35bc26f8afd7
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-10-01 19:15:50 +0200

    Fix indentation

--- ex1/hardware/registers.vhd
+++ ex1/hardware/registers.vhd
@@ -3,15 +3,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity registers is
-    Port ( clk : in STD_LOGIC;
-           reset : in STD_LOGIC;
-           read_register_1_in : in  STD_LOGIC_VECTOR (4 downto 0);
-           read_register_2_in : in  STD_LOGIC_VECTOR (4 downto 0);
-           write_register_in : in  STD_LOGIC_VECTOR (4 downto 0);
-           write_data_in : in  SIGNED (31 downto 0);
-           reg_write_in : in  STD_LOGIC;
-           read_data_1_out : out  SIGNED (31 downto 0);
-           read_data_2_out : out  SIGNED (31 downto 0));
+  Port ( clk : in STD_LOGIC;
+         reset : in STD_LOGIC;
+         read_register_1_in : in  STD_LOGIC_VECTOR (4 downto 0);
+         read_register_2_in : in  STD_LOGIC_VECTOR (4 downto 0);
+         write_register_in : in  STD_LOGIC_VECTOR (4 downto 0);
+         write_data_in : in  SIGNED (31 downto 0);
+         reg_write_in : in  STD_LOGIC;
+         read_data_1_out : out  SIGNED (31 downto 0);
+         read_data_2_out : out  SIGNED (31 downto 0));
 end registers;
 
 architecture Behavioral of registers is
