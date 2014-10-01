1f6ebf1308cee3c92ffd7f9a2fd7b34e90af7c85
Stian Jensen <me@stianj.com>
2014-10-01 22:53:15 +0200

    Implement memory unit

--- ex1/hardware/memory_unit.vhd
+++ ex1/hardware/memory_unit.vhd
@@ -10,6 +10,7 @@ entity memory_unit is
          pc_in : in  STD_LOGIC_VECTOR (31 downto 0);
          alu_out_in : in  STD_LOGIC_VECTOR (31 downto 0);
          mem_data_out : out  STD_LOGIC_VECTOR (31 downto 0);
+         write_data_in : in STD_LOGIC_VECTOR (31 downto 0);
          clk : in  STD_LOGIC;
          reset : in  STD_LOGIC;
          imem_data_in : in  STD_LOGIC_VECTOR (31 downto 0);
@@ -24,6 +25,31 @@ architecture Behavioral of memory_unit is
 
 begin
 
+  process (clk) is
+  begin
+    if rising_edge(clk) then
+      if reset = '1' then
+        -- bluh
+      end if;
+
+      if control_i_or_d = '1' then
+        dmem_address_out <= alu_out_in(7 downto 0);
+      else
+        imem_address_out <= pc_in(7 downto 0);
+      end if;
+
+      if control_mem_write = '1' then
+        dmem_write_enable_out <= '1';
+        dmem_data_out <= write_data_in;
+      end if;
+
+      if control_mem_read = '1' then
+        mem_data_out <= dmem_data_in;
+      else
+        mem_data_out <= imem_data_in;
+      end if;
+    end if;
+  end process;
 
 end Behavioral;
 
