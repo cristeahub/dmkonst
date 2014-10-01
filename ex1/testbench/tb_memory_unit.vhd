60289f6e069499413c74a05f773b456c98c364b6
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2014-10-01 22:58:30 +0200

    Add first iteration memory unit

--- ex1/testbench/tb_memory_unit.vhd
+++ ex1/testbench/tb_memory_unit.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use work.test_util.all;
 --use ieee.numeric_std.all;
 
 entity tb_memory_unit is
@@ -16,6 +17,7 @@ architecture behavior of tb_memory_unit is
           pc_in : in  std_logic_vector(31 downto 0);
           alu_out_in : in  std_logic_vector(31 downto 0);
           mem_data_out : out  std_logic_vector(31 downto 0);
+          write_data_in : in std_logic_vector(31 downto 0);
           clk : in  std_logic;
           reset : in  std_logic;
           imem_data_in : in  std_logic_vector(31 downto 0);
@@ -33,6 +35,7 @@ architecture behavior of tb_memory_unit is
   signal control_mem_write : std_logic := '0';
   signal pc_in : std_logic_vector(31 downto 0) := (others => '0');
   signal alu_out_in : std_logic_vector(31 downto 0) := (others => '0');
+  signal write_data_in : std_logic_vector(31 downto 0) := (others => '0');
   signal clk : std_logic := '0';
   signal reset : std_logic := '0';
   signal imem_data_in : std_logic_vector(31 downto 0) := (others => '0');
@@ -58,6 +61,7 @@ begin
                               pc_in => pc_in,
                               alu_out_in => alu_out_in,
                               mem_data_out => mem_data_out,
+                              write_data_in => write_data_in,
                               clk => clk,
                               reset => reset,
                               imem_data_in => imem_data_in,
@@ -82,13 +86,40 @@ begin
   stim_proc: process
   begin		
     -- hold reset state for 100 ns.
-    wait for 100 ns;	
+    wait for 100 ns;
 
     wait for clk_period*10;
 
-    -- insert stimulus here 
+    -- read from memory
+    control_i_or_d <= '1';
+    control_mem_read <= '1';
+    alu_out_in <= x"abbababb";
+    dmem_data_in <= x"cafebabe";
+    wait for clk_period;
+    assert_equals(x"cafebabe", mem_data_out, "Testing memory read data correctly");
+    assert_equals(x"bb", dmem_address_out, "Testing memory address correctly");
+    wait for clk_period*10;
+
+    -- read instruction
+    control_i_or_d <= '0';
+    pc_in <= x"decec00c";
+    imem_data_in <= x"ccccdddd";
+    wait for clk_period;
+    assert_equals(x"ccccdddd", mem_data_out, "Testing instruction data out");
+    assert_equals(x"0c", imem_address_out, "Testing memory address correctly");
+    wait for clk_period*10;
+
+    -- write data
+    control_i_or_d <= '1';
+    control_mem_write <= '1';
+    alu_out_in <= x"00000011";
+    write_data_in <= x"dd00dd00";
+    wait for clk_period;
+    assert_equals(x"dd00dd00", dmem_data_out, "Testing write data");
+    assert_equals(x"11", dmem_address_out, "Testing write address data");
+    wait for clk_period*10;
 
     wait;
   end process;
 
-eND;
+end;
