f58930a1efe84a11ea4b10aa73aa405978cf53a3
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-10-01 19:12:11 +0200

    Add register testbench and implementation skeleton

--- ex1/testbench/tb_registers.vhd
+++ ex1/testbench/tb_registers.vhd
@@ -1,54 +1,81 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.test_utils.all;
-use work.constants.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
-entity tb_registers is
-end tb_registers;
+ENTITY tb_registers IS
+  END tb_registers;
 
-architecture behavior of tb_registers is 
+ARCHITECTURE behavior OF tb_registers IS 
 
-  component alu
-    port(
-          read_reg_one_in : in  signed(25 downto 21);
-          read_reg_two_in : in  signed(20 downto 16);
-          write_reg_in : in  std_logic_vector(15 downto 11);
-          write_data_in : in  std_logic;
-          read_data_one_out : out  signed(31 downto 0)
-          read_data_two_out : out  signed(31 downto 0)
+    -- Component Declaration for the Unit Under Test (UUT)
+
+  COMPONENT registers
+    PORT(
+          clk : IN  std_logic;
+          reset : IN  std_logic;
+          read_register_1_in : IN  std_logic_vector(4 downto 0);
+          read_register_2_in : IN  std_logic_vector(4 downto 0);
+          write_register_in : IN  std_logic_vector(4 downto 0);
+          write_data_in : IN  SIGNED(31 downto 0);
+          reg_write_in : IN  std_logic;
+          read_data_1_out : OUT  SIGNED(31 downto 0);
+          read_data_2_out : OUT  SIGNED(31 downto 0)
         );
-  end component;
+  END COMPONENT;
 
-  --inputs
-  signal read_reg_one_in : signed(25 downto 21) := (others => '0');
-  signal read_reg_two_in : signed(20 downto 16) := (others => '0');
-  signal write_reg_in : std_logic_vector(15 downto 11) := (others => '0');
-  signal write_data_in : std_logic := '0';
 
-  --outputs
-  signal read_data_one_out : signed(31 downto 0) := (others => '0');
-  signal read_data_two_out : signed(31 downto 0) := (others => '0');
+   --Inputs
+  signal clk : std_logic := '0';
+  signal reset : std_logic := '0';
+  signal read_register_1_in : std_logic_vector(4 downto 0) := (others => '0');
+  signal read_register_2_in : std_logic_vector(4 downto 0) := (others => '0');
+  signal write_register_in : std_logic_vector(4 downto 0) := (others => '0');
+  signal write_data_in : SIGNED(31 downto 0) := (others => '0');
+  signal reg_write_in : std_logic := '0';
 
-begin
+   --Outputs
+  signal read_data_1_out : SIGNED(31 downto 0);
+  signal read_data_2_out : SIGNED(31 downto 0);
 
-  -- instantiate the unit under test (uut)
-  uut: registers port map (
-                            read_reg_one_in => read_reg_one_in,
-                            read_reg_two_in => read_reg_two_in,
-                            write_reg_in => write_reg_in,
+   -- Clock period definitions
+  constant clk_period : time := 10 ns;
+
+BEGIN
+
+   -- Instantiate the Unit Under Test (UUT)
+  uut: registers PORT MAP (
+                            clk => clk,
+                            reset => reset,
+                            read_register_1_in => read_register_1_in,
+                            read_register_2_in => read_register_2_in,
+                            write_register_in => write_register_in,
                             write_data_in => write_data_in,
-                            read_data_one_out => read_data_one_out,
-                            read_data_two_out => read_data_two_out
+                            reg_write_in => reg_write_in,
+                            read_data_1_out => read_data_1_out,
+                            read_data_2_out => read_data_2_out
                           );
 
-  -- stimulus process
-  stim_proc: process
-  begin		
-    -- hold reset state for 100 ns.
-    wait for 100 ns;
-
-    -- check that values out is what is asked for in
+   -- Clock process definitions
+  clk_process :process
+  begin
+    clk <= '0';
+    wait for clk_period/2;
+    clk <= '1';
+    wait for clk_period/2;
   end process;
 
-end;
+
+   -- Stimulus process
+  stim_proc: process
+  begin		
+      -- hold reset state for 100 ns.
+    wait for 100 ns;	
+
+    wait for clk_period*10;
+
+      -- insert stimulus here 
+
+    wait;
+  end process;
+
+END;
