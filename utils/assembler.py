0c806c374871d4f49a06fe82320ef5c1dd6f0135
Stian Jensen <me@stianj.com>
2014-11-02 20:57:00 +0100

    Add assembler

---
+++ utils/assembler.py
@@ -0,0 +1,83 @@
+from sys import stdin
+
+opcodes = {
+    'lw': 0x23,
+    'sw': 0x2b,
+    'beq': 0x4,
+}
+instruction_formats = {
+    'r': ['add', 'sub'],
+    'i': ['lw', 'sw', 'beq'],
+    'j': ['j'],
+    'nop': ['nop'],
+}
+
+funct_codes = {
+    'add': 0x20,
+    'sub': 0x22,
+    'and': 0x24,
+    'or': 0x25,
+    'slt': 0x2a,
+    'sll': 0x00,
+}
+
+
+def parse_r_type(instruction_tokens):
+    instruction = instruction_tokens[0]
+    opcode = 0
+
+    alu_function = funct_codes[instruction]
+
+    rd, rt, rs = map(lambda e: int(e[1:]), instruction_tokens[1:])
+
+    shamt = 0
+
+    return (opcode << 26) + (rs << 21) + (rt << 16) + (rd << 11) + (shamt << 6) + alu_function
+
+
+def parse_i_type(instruction_tokens):
+    instruction = instruction_tokens[0]
+    opcode = opcodes[instruction]
+
+    rt = int(instruction_tokens[1][1:])
+
+    imm, rs = instruction_tokens[2].split('($')
+
+    imm = int(imm)
+    rs = int(rs[:-1])
+
+    return (opcode << 26) + (rs << 21) + (rt << 16) + imm
+
+
+def parse_j_type(instruction_tokens):
+    pass
+
+
+program = []
+
+for raw_instruction in stdin:
+    instruction_tokens = raw_instruction.strip().replace(',', '').split(' ')
+    instruction = instruction_tokens[0]
+
+    instruction_format = ''
+    for key, value in instruction_formats.iteritems():
+        if instruction in value:
+            instruction_format = key
+            break
+
+    encoded_instruction = 0
+    if instruction_format == 'r':
+        encoded_instruction = parse_r_type(instruction_tokens)
+    if instruction_format == 'i':
+        encoded_instruction = parse_i_type(instruction_tokens)
+    if instruction_format == 'j':
+        encoded_instruction = parse_j_type(instruction_tokens)
+    if instruction_format == 'nop':
+        encoded_instruction = parse_r_type(['sll', '$0', '$0', '$0'])
+
+    program.append('X"{:08x}", -- {}'.format(encoded_instruction, raw_instruction))
+
+
+program[-1] = program[-1].replace(',', '', 1)
+
+print ''.join(program)
