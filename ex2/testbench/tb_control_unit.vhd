a596c4cd8442446dda2a345b2b475de1e7beb040
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2014-11-04 15:48:37 -0800

    Add control unit test bench

--- ex2/testbench/tb_control_unit.vhd
+++ ex2/testbench/tb_control_unit.vhd
@@ -1,238 +1,152 @@
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-USE work.test_utils.ALL;
-USE work.constants.ALL;
 
+USE ieee.numeric_std.ALL;
+ 
 ENTITY tb_control_unit IS
 END tb_control_unit;
  
 ARCHITECTURE behavior OF tb_control_unit IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT control_unit
+    PORT(
+         instruction_in : IN  std_logic_vector(31 downto 26);
+         processor_enable : IN  std_logic;
+         reg_dst_out : OUT  std_logic;
+         alu_op_out : OUT  std_logic_vector(1 downto 0);
+         alu_src_out : OUT  std_logic;
+         branch_out : OUT  std_logic;
+         mem_write_out : OUT  std_logic;
+         reg_write_out : OUT  std_logic;
+         mem_to_reg_out : OUT  std_logic;
+         pc_jump_override_out : OUT  std_logic
+        );
+    END COMPONENT;
+    
 
    --Inputs
-   signal clk : std_logic := '0';
-   signal reset : std_logic := '0';
    signal instruction_in : std_logic_vector(31 downto 26) := (others => '0');
-   signal processor_enable : std_logic;
+   signal processor_enable : std_logic := '0';
 
  	--Outputs
-   signal ir_write : std_logic;
-   signal pc_write : std_logic;
-   signal pc_write_cond : std_logic;
-   signal pc_source : std_logic_vector(1 downto 0);
-   signal mem_to_reg : std_logic;
-   signal alu_op : std_logic_vector(1 downto 0);
-   signal mem_write : std_logic;
-   signal alu_src_a : std_logic;
-   signal alu_src_b : std_logic_vector(1 downto 0);
-   signal reg_write : std_logic;
-   signal reg_dst : std_logic;
-
-   -- Clock period definitions
+   signal reg_dst_out : std_logic;
+   signal alu_op_out : std_logic_vector(1 downto 0);
+   signal alu_src_out : std_logic;
+   signal branch_out : std_logic;
+   signal mem_write_out : std_logic;
+   signal reg_write_out : std_logic;
+   signal mem_to_reg_out : std_logic;
+   signal pc_jump_override_out : std_logic;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
    constant clk_period : time := 10 ns;
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
-   uut: entity work.control_unit PORT MAP (
-          clk => clk,
-          reset => reset,
+   uut: control_unit PORT MAP (
           instruction_in => instruction_in,
           processor_enable => processor_enable,
-          ir_write => ir_write,
-          pc_write => pc_write,
-          pc_write_cond => pc_write_cond,
-          pc_source => pc_source,
-          mem_to_reg => mem_to_reg,
-          alu_op => alu_op,
-          mem_write => mem_write,
-          alu_src_a => alu_src_a,
-          alu_src_b => alu_src_b,
-          reg_write => reg_write,
-          reg_dst => reg_dst
+          reg_dst_out => reg_dst_out,
+          alu_op_out => alu_op_out,
+          alu_src_out => alu_src_out,
+          branch_out => branch_out,
+          mem_write_out => mem_write_out,
+          reg_write_out => reg_write_out,
+          mem_to_reg_out => mem_to_reg_out,
+          pc_jump_override_out => pc_jump_override_out
         );
 
-   -- Clock process definitions
-   clk_process :process
-   begin
-     clk <= '0';
-     wait for clk_period/2;
-     clk <= '1';
-     wait for clk_period/2;
-   end process;
- 
 
    -- Stimulus process
    stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
 
-     procedure check_state_instruction_fetch is
-     begin
-       assert_equals("01", alu_src_b, "alu_src_b");
-       assert_equals('1', pc_write, "pc_write");
-     end procedure check_state_instruction_fetch;
+      wait for clk_period*10;
 
-     procedure check_state_instruction_decode is
-     begin
-       assert_equals("10", alu_src_b, "alu_src_b should be 10");
-       assert_equals('1', ir_write, "ir_write");
-     end procedure check_state_instruction_decode;
+		processor_enable <= '1';
 
-     procedure check_state_memory_address_computation is
-     begin
-       assert_equals('1', alu_src_a, "");
-       assert_equals("10", alu_src_b, "");
-       assert_equals("00", alu_op, "");
-     end procedure check_state_memory_address_computation;
+		instruction_in <= R_TYPE;
 
-   begin
+		wait for clk_period;
+
+		assert_equals('1', reg_dst_out, "reg_dst_out is on");
+		assert_equals('1', reg_write_out, "reg_write_out is on");
+		assert_equals("10", alu_op_out, "alu_op_out is 10");
+
+		instruction_in <= LW;
+
+		wait for clk_period;
+
+		assert_equals('1', alu_src_out, "alu_src_out is on");
+		assert_equals('1', reg_write_out, "reg_write_out is on");
+		assert_equals('1', mem_to_reg_out, "mem_to_reg_out is on");
+
+		instruction_in <= SW;
+
+		wait for clk_period;
+
+		assert_equals('1', alu_src_out, "alu_src_out is on");
+		assert_equals('1', mem_write_out, "mem_write_out is on");
+
+		instruction_in <= LUI;
+
+		wait for clk_period;
+
+		assert_equals("11", alu_op_out, "alu_op_out is 11");
+		assert_equals('1', reg_write_out, "reg_write_out is on");
+		assert_equals('1', alu_src_out, "alu_src_out is on");
+
+		instruction_in <= BRANCH;
+
+		wait for clk_period;
+
+		assert_equals("01", alu_op_out, "alu_op_out is 01");
+		assert_equals('1', branch_out, "branch_out is on");
+
+		instruction_in <= JUMP;
+
+		wait for clk_period;
+
+		assert_equals('1', pc_jump_override_out, "pc_jump_override_out is on");
+
+		-- turn off processor_enable
+		-- do a random sample of tests
       processor_enable <= '0';
-      reset <= '1';
 
-      -- Testing path R_TYPE
+		assert_equals('0', reg_dst_out, "reg_dst_out is off");
+		assert_equals('0', reg_write_out, "reg_write_out is off");
+		assert_equals("00", alu_op_out, "alu_op_out is off");
 
-      wait for clk_period;
+		instruction_in <= LW;
 
-      reset <= '0';
+		wait for clk_period;
 
-      wait for clk_period;
+		assert_equals('0', alu_src_out, "alu_src_out is off");
+		assert_equals('0', reg_write_out, "reg_write_out is off");
+		assert_equals('0', mem_to_reg_out, "mem_to_reg_out is off");
 
-      assert_equals('0', pc_write, "Processor should not be enabled");
+		instruction_in <= SW;
 
-      processor_enable <= '1';
+		wait for clk_period;
 
-      wait for clk_period;
+		assert_equals('0', alu_src_out, "alu_src_out is off");
+		assert_equals('0', mem_write_out, "mem_write_out is off");
 
-      check_state_instruction_fetch;
+		instruction_in <= LUI;
 
-      wait for clk_period;
+		wait for clk_period;
 
-      check_state_instruction_decode;
+		assert_equals("00", alu_op_out, "alu_op_out is off");
+		assert_equals('0', reg_write_out, "reg_write_out is of");
+		assert_equals('0', alu_src_out, "alu_src_out is off");
 
-      instruction_in <= R_TYPE;
-
-      wait for clk_period;
-
-      assert_equals('1', alu_src_a, "alu_src_a should be 1");
-      assert_equals("00", alu_src_b, "alu_src_b should be 00");
-      assert_equals("10", alu_op, "alu_op should be 10");
-
-      wait for clk_period;
-
-      assert_equals('1', reg_dst, "reg_dst should be 1");
-      assert_equals('1', reg_write, "reg_write should be 1");
-      assert_equals('0', mem_to_reg, "mem_to_reg should be 0");
-
-      wait for clk_period;
-
-      -- Testing path JUMP
-
-      check_state_instruction_fetch;
-
-      wait for clk_period;
-
-      check_state_instruction_decode;
-
-      instruction_in <= JUMP;
-
-      wait for clk_period;
-
-      assert_equals('1', pc_write, "");
-      assert_equals("10", pc_source, "");
-
-      wait for clk_period;
-
-      -- Testing BRANCH
-
-      check_state_instruction_fetch;
-
-      wait for clk_period;
-
-      check_state_instruction_decode;
-
-      instruction_in <= BRANCH;
-
-      wait for clk_period;
-
-      assert_equals('1', alu_src_a, "");
-      assert_equals("00", alu_src_b, "");
-      assert_equals("01", alu_op, "");
-      assert_equals('1', pc_write_cond, "");
-      assert_equals("01", pc_source, "");
-
-      wait for clk_period;
-
-      -- Testing LOAD
-
-      check_state_instruction_fetch;
-
-      wait for clk_period;
-
-      check_state_instruction_decode;
-
-      instruction_in <= LW;
-
-      wait for clk_period;
-
-      check_state_memory_address_computation;
-
-      wait for clk_period;
-
-      assert_equals('0', mem_write, "");
-
-      wait for clk_period;
-
-      assert_equals('0', reg_dst, "");
-      assert_equals('1', reg_write, "");
-      assert_equals('1', mem_to_reg, "");
-
-      wait for clk_period;
-
-      -- Testing SW
-
-      check_state_instruction_fetch;
-
-      wait for clk_period;
-
-      check_state_instruction_decode;
-
-      instruction_in <= SW;
-
-      wait for clk_period;
-
-      check_state_memory_address_computation;
-
-      wait for clk_period;
-
-      assert_equals('1', mem_write, "");
-
-      wait for clk_period;
-
-      -- Testing LUI
-
-      check_state_instruction_fetch;
-
-      wait for clk_period;
-
-      check_state_instruction_decode;
-
-      instruction_in <= LUI;
-
-      wait for clk_period;
-
-      assert_equals('0', alu_src_a, "alu_src_a");
-      assert_equals("10", alu_src_b, "alu_src_b");
-      assert_equals("11", alu_op, "alu_op");
-
-      wait for clk_period;
-
-      assert_equals('1', reg_write, "reg_write");
-
-      wait for clk_period;
-
-      -- Should return to instruction fetch
-
-      check_state_instruction_fetch;
-
-      report "Test complete";
 
       wait;
    end process;
