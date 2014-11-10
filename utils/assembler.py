1500a934e48c0244d75f7ffc35139f84f86a7766
Stian Jensen <me@stianj.com>
2014-11-10 16:17:46 +0100

    Add hostcomm support to assembler

--- utils/assembler.py
+++ utils/assembler.py
@@ -1,27 +1,28 @@
 from sys import stdin
+import argparse
 
 opcodes = {
-    'lw': 0x23,
-    'sw': 0x2b,
-    'beq': 0x4,
-    'lui': 0xf,
-}
+        'lw': 0x23,
+        'sw': 0x2b,
+        'beq': 0x4,
+        'lui': 0xf,
+        }
 instruction_formats = {
-    'r': ['add', 'sub', 'and', 'or', 'slt', 'sll', 'srl'],
-    'i': ['lw', 'sw', 'beq', 'lui'],
-    'j': ['j'],
-    'nop': ['nop'],
-}
+        'r': ['add', 'sub', 'and', 'or', 'slt', 'sll', 'srl'],
+        'i': ['lw', 'sw', 'beq', 'lui'],
+        'j': ['j'],
+        'nop': ['nop'],
+        }
 
 funct_codes = {
-    'add': 0x20,
-    'sub': 0x22,
-    'and': 0x24,
-    'or': 0x25,
-    'slt': 0x2a,
-    'sll': 0x00,
-    'srl': 0x02,
-}
+        'add': 0x20,
+        'sub': 0x22,
+        'and': 0x24,
+        'or': 0x25,
+        'slt': 0x2a,
+        'sll': 0x00,
+        'srl': 0x02,
+        }
 
 
 def parse_r_type(instruction_tokens):
@@ -72,29 +73,53 @@ def parse_j_type(instruction_tokens):
     return "{:08X}".format((opcode << 26) + address)
 
 
-program = []
+def parse(source):
+    program = []
+    for raw_instruction in source:
+        instruction_tokens = raw_instruction.strip().replace(',', '').split(' ')
+        instruction = instruction_tokens[0]
 
-for raw_instruction in stdin:
-    instruction_tokens = raw_instruction.strip().replace(',', '').split(' ')
-    instruction = instruction_tokens[0]
+        instruction_format = ''
+        for key, value in instruction_formats.iteritems():
+            if instruction in value:
+                instruction_format = key
+                break
 
-    instruction_format = ''
-    for key, value in instruction_formats.iteritems():
-        if instruction in value:
-            instruction_format = key
-            break
+        encoded_instruction = "00000000"  # Default to nop
+        if instruction_format == 'r':
+            encoded_instruction = parse_r_type(instruction_tokens)
+        if instruction_format == 'i':
+            encoded_instruction = parse_i_type(instruction_tokens)
+        if instruction_format == 'j':
+            encoded_instruction = parse_j_type(instruction_tokens)
 
-    encoded_instruction = "00000000"  # Default to nop
-    if instruction_format == 'r':
-        encoded_instruction = parse_r_type(instruction_tokens)
-    if instruction_format == 'i':
-        encoded_instruction = parse_i_type(instruction_tokens)
-    if instruction_format == 'j':
-        encoded_instruction = parse_j_type(instruction_tokens)
-
-    program.append('X"{}", -- {}'.format(encoded_instruction, raw_instruction))
+        program.append([encoded_instruction, raw_instruction])
+    return program
 
 
-program[-1] = program[-1].replace(',', '', 1)
+def change_endianness(inst):
+    out = ""
+    pairs = map(lambda x,y: y+x, inst[0:][::2], inst[1:][::2])
+    out += "".join(pairs)[::-1]
+    return out
 
-print ''.join(program)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="MIPS Assembler")
+    parser.add_argument('--hostcomm', dest='hostcomm', action='store_true', help='Will reverse endianness, and produce bare hex output')
+    args = parser.parse_args()
+
+    program = parse(stdin)
+
+    if args.hostcomm:
+        output = ""
+        for encoded_instruction, _ in program:
+            output += change_endianness(encoded_instruction)
+        print output
+    else:
+        output = []
+        for encoded_instruction, raw_instruction in program:
+            output.append('X"{}", -- {}'.format(encoded_instruction, raw_instruction))
+
+        output[-1] = output[-1].replace(',', '', 1)
+
+        print ''.join(output)
